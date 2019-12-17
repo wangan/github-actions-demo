7a3b04890e9f218c49dcf13a65235e2d596581fd
wangan <anntony.wang@live.com>
2019-12-17 09:49:38 +0800

    Updates

---
+++ precache-manifest.a18e63b6941115975462d7b05c09a8d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c48a2f8f753a639204a7b3c39aceda57",
+    "url": "/github-actions-demo/index.html"
+  },
+  {
+    "revision": "37c185fdb4794f46f7ad",
+    "url": "/github-actions-demo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1317868cc3c6e6b74ee4",
+    "url": "/github-actions-demo/static/js/2.b6928ad3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/github-actions-demo/static/js/2.b6928ad3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "37c185fdb4794f46f7ad",
+    "url": "/github-actions-demo/static/js/main.79a29b63.chunk.js"
+  },
+  {
+    "revision": "f0f33a78e395284a4850",
+    "url": "/github-actions-demo/static/js/runtime-main.6d112104.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-actions-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
